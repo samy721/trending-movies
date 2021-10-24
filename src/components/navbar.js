19e4ca3056102c96a6a4a1e16fed72d71913c89f
Shekhar Mourya <sm.contactmeon@gmail.com>
2021-10-24 16:57:36 +0530

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,7 +5,7 @@ export default class Navbar extends Component {
     render() {
         return (
             <div className='container site-title'>
-                <Link to='/' style={{textDecoration:'none', marginRight:'20px'}} ><h1>Trending Movies </h1></Link>
+                <Link to='/trending-movies' style={{textDecoration:'none', marginRight:'20px'}} ><h1>Trending Movies </h1></Link>
                 <Link to='/watchlist' style={{textDecoration:'none'}} ><h1> Watch List  ❤ </h1></Link>
             </div>
         )
