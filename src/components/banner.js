6ba0fdfe87829b3573d8b1244255fddcc85e1a91
Shekhar Mourya <sm.contactmeon@gmail.com>
2021-11-07 18:45:54 +0530

    Added link to icon

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 export default class banner extends Component {
         constructor() {
@@ -26,10 +27,11 @@ export default class banner extends Component {
         { 
         topmovies.slice(0, Math.floor(Math.random() * 15)).map( item => (
                 <div class=" carousel-item active">
-                          <img class="bd-placeholder-img" width="100%" src={`https://image.tmdb.org/t/p/original${item.backdrop_path}`} aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false" />
-                          <div class="container">
+                          <img class="bd-placeholder-img" width="100%" src={`https://image.tmdb.org/t/p/original${item.backdrop_path}`} aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false" alt="banner-img" />
+                        <div class="container banner-title ">
                                 <div class="carousel-caption">
-                                  <h2 class="banner-title" >Trending Now : "{item.original_title}"</h2>
+                                  <h2 class="" >Trending Now: "{item.original_title}"</h2>
+                                  <Link to='/trending-movies' style={{color:'white'}}><p>Next</p></Link>
                                 </div>
                         </div>
                 </div>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,7 +5,7 @@ export default class Navbar extends Component {
     render() {
         return (
             <div className='container-fluid nav-title'>
-                <img width='70px' src="https://play-lh.googleusercontent.com/ZKxqgazA73cF7wdXn1KFZaEGFPLCSm2VvmHTFPtuobIWsk6d9Jvuxr68CGrDLDykBfLN=rw-w250" alt="logo" />
+                <Link to='/trending-movies'><img width='70px' src="https://play-lh.googleusercontent.com/ZKxqgazA73cF7wdXn1KFZaEGFPLCSm2VvmHTFPtuobIWsk6d9Jvuxr68CGrDLDykBfLN=rw-w250" alt="logo" /></Link>
                 <Link to='/trending-movies' style={{textDecoration:'none'}} ><h1> TrendingMovies.com </h1></Link>
                 <Link to='/favouritelist' style={{textDecoration:'none'}} ><h4 style={{color:'red'}}>❤</h4><span>Fav</span></Link>
             </div>
